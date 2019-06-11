e4014d14dd0d6a9f8e42775bab9ebc720ddf986d
Bastian Kruck <bkruck@itemis.com>
2019-06-11 16:11:42 +0200

    test.modelcheck: bump versions to 1.2-SNAPSHOT and MPS 2018.3.6

--- test/modelcheck/build.gradle.kts
+++ test/modelcheck/build.gradle.kts
@@ -1,14 +1,14 @@
 import java.net.URI
 
 group = "test.de.itemis.mps.gradle.modelcheck"
-version = "1.1-SNAPSHOT"
+version = "1.2-SNAPSHOT"
 
 if(JavaVersion.current() != JavaVersion.VERSION_1_8){
     throw GradleException("This build script requires java " + JavaVersion.VERSION_1_8 + ", but you are currently using " + JavaVersion.current())
 }
 
 plugins {
-    id("modelcheck") version "1.1-SNAPSHOT"
+    id("modelcheck") version "1.2-SNAPSHOT"
 }
 
 repositories {
@@ -23,7 +23,7 @@ var mps = configurations.create("mps")
 
 
 dependencies{
-    mps("com.jetbrains:mps:2018.2.5")
+    mps("com.jetbrains:mps:2018.3.6")
 }
 
 modelcheck {
