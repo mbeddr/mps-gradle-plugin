fc0674b8b2903663befe4b1e78c6612cd5e79214
Norman Köster <nkoester@itemis.com>
2021-09-10 14:13:34 +0200

    Build: Adapt ghp publication changes to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ version = if (!project.hasProperty("useSnapshot") &&
     "$versionMajor.$versionMinor-SNAPSHOT"
 }
 
+var currentBranch:String? = ""
+currentBranch = de.itemis.mps.gradle.GitBasedVersioning.getGitBranch()
 
 val mpsConfiguration = configurations.create("mps")
 
@@ -103,6 +105,20 @@ publishing {
             credentials {
                 username = nexusUsername
                 password = nexusPassword
+            }
+        }
+    }
+    repositories {
+        if(currentBranch == "master" || currentBranch!!.startsWith("mps")) {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/mbeddr/mps-gradle-plugin")
+                if(project.hasProperty("gpr.token")) {
+                    credentials {
+                        username = project.findProperty("gpr.user") as String
+                        password = project.findProperty("gpr.token") as String
+                    }
+                }
             }
         }
     }
