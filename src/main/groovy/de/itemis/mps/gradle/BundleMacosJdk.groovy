e973e08e45c8f8036a0cbe2c8f4f93916dffe5e1
Sergej Koščejev <sergej@koscejev.cz>
2021-11-15 12:09:13 +0100

    BundleMacosJdk: add missing annotation

--- src/main/groovy/de/itemis/mps/gradle/BundleMacosJdk.groovy
+++ src/main/groovy/de/itemis/mps/gradle/BundleMacosJdk.groovy
@@ -3,6 +3,7 @@ package de.itemis.mps.gradle
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.artifacts.Dependency
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
@@ -12,7 +13,7 @@ class BundleMacosJdk extends DefaultTask {
     @InputFile
     File rcpArtifact
 
-    @Optional
+    @Optional @Input
     String jdkDirname  = "jre"
 
     @InputFile
