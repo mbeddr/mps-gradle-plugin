c202f0f6fa6f8da8b0059b40722d1e122feddcad
Sergej Koščejev <sergej@koscejev.cz>
2021-11-15 11:36:34 +0100

    Add `incremental` flag to RunAntScript, bump minor version
    Having an explicit flag for controlling incrementality makes it a little
    easier to use. It also lets us remove the `clean` target from the
    invoked targets, this is something that those who configure incremental
    builds manually often forget, thus making their builds not quite as
    incremental as could be possible.
    
    Since this is a new feature, the minor version is increased.

--- src/main/kotlin/de/itemis/mps/gradle/RunAntScript.kt
+++ src/main/kotlin/de/itemis/mps/gradle/RunAntScript.kt
@@ -25,6 +25,19 @@ open class RunAntScript : DefaultTask() {
     @Optional @Input
     var executable: Any? = null
 
+    /**
+     * Whether to build incrementally.
+     *
+     * Possible values:
+     * * `true` - perform an incremental build. If the [targets] list includes `clean` target it will be removed, and
+     *   `-Dmps.generator.skipUnmodifiedModels=true` will be passed to Ant.
+     * * `false` - The backwards compatible default. The [targets] list will not be modified and no properties will be
+     *   passed to Ant. Any outside customizations made to targets and Ant arguments are left intact so the build may
+     *   in fact be incremental.
+     */
+    @Input
+    var incremental: Boolean = false
+
     fun targets(vararg targets: String) {
         this.targets = targets.toList()
     }
@@ -35,17 +48,23 @@ open class RunAntScript : DefaultTask() {
 
     @TaskAction
     fun build() {
-        var allArgs = scriptArgs
+        val allArgs = scriptArgs.toMutableList()
         if (includeDefaultArgs) {
             val defaultArgs = project.findProperty("itemis.mps.gradle.ant.defaultScriptArgs") as Collection<*>?
             if (defaultArgs != null) {
-                allArgs = allArgs + defaultArgs.map { it as String }
+                allArgs += defaultArgs.map { it as String }
             }
         }
 
         if(logging.level != null && logging.level != LogLevel.LIFECYCLE && !allArgs.any { it.startsWith("-Dmps.ant.log=") }) {
-            allArgs = allArgs + "-Dmps.ant.log=${logging.level.toString().toLowerCase(Locale.ENGLISH)}"
+            allArgs += "-Dmps.ant.log=${logging.level.toString().toLowerCase(Locale.ENGLISH)}"
         }
+
+        if (incremental) {
+            allArgs += "-Dmps.generator.skipUnmodifiedModels=true"
+        }
+
+        val targets = if (incremental) { targets - "clean" } else { targets }
 
         project.javaexec {
             if (this@RunAntScript.executable != null) {
