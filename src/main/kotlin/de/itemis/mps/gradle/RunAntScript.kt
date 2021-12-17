6136c66a3ecca2edebb31a34c28d385c3fd3e74a
dbinkele <dbinkele@itemis.de>
2021-12-17 11:22:33 +0100

    Moving to Property 1

--- src/main/kotlin/de/itemis/mps/gradle/RunAntScript.kt
+++ src/main/kotlin/de/itemis/mps/gradle/RunAntScript.kt
@@ -1,110 +1,129 @@
 package de.itemis.mps.gradle
 
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.LogLevel
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import java.util.*
+import javax.inject.Inject
 
-open class RunAntScript : DefaultTask() {
+open class RunAntScript @Inject constructor(of: ObjectFactory) : DefaultTask() {
     @Input
     lateinit var script: Any
+
     @Input
     var targets: List<String> = emptyList()
-    @Optional @InputFiles
+
+    @Optional
+    @InputFiles
     var scriptClasspath: FileCollection? = null
+
     @Input
     var scriptArgs: List<String> = emptyList()
+
+    @Optional
     @Input
-    var includeDefaultArgs = true
+    val includeDefaultArgs: Property<Boolean> = of.property(Boolean::class.java)
+
+    @Optional
     @Input
-    var includeDefaultClasspath = true
-    @Optional @Input
-    var executable: Any? = null
+    val includeDefaultClasspath: Property<Boolean> = of.property(Boolean::class.java)
 
-    /**
-     * Whether to build incrementally.
-     *
-     * Possible values:
-     * * `true` - perform an incremental build. If the [targets] list includes `clean` target it will be removed, and
-     *   `-Dmps.generator.skipUnmodifiedModels=true` will be passed to Ant.
-     * * `false` - The backwards compatible default. The [targets] list will not be modified and no properties will be
-     *   passed to Ant. Any outside customizations made to targets and Ant arguments are left intact so the build may
-     *   in fact be incremental.
-     */
+    @Optional
     @Input
-    var incremental: Boolean = false
+    val executable: Property<Any> = of.property(Any::class.java)
 
-    fun targets(vararg targets: String) {
-        this.targets = targets.toList()
-    }
+        /**
+         * Whether to build incrementally.
+         *
+         * Possible values:
+         * * `true` - perform an incremental build. If the [targets] list includes `clean` target it will be removed, and
+         *   `-Dmps.generator.skipUnmodifiedModels=true` will be passed to Ant.
+         * * `false` - The backwards compatible default. The [targets] list will not be modified and no properties will be
+         *   passed to Ant. Any outside customizations made to targets and Ant arguments are left intact so the build may
+         *   in fact be incremental.
+         */
+        @Input
+        var incremental: Boolean = false
 
-    fun executable(executable: Any?) {
-        this.executable = executable
-    }
+        fun targets(vararg targets: String) {
+            this.targets = targets.toList()
+        }
 
-    @TaskAction
-    fun build() {
-        val allArgs = scriptArgs.toMutableList()
-        if (includeDefaultArgs) {
-            val defaultArgs = project.findProperty("itemis.mps.gradle.ant.defaultScriptArgs") as Collection<*>?
-            if (defaultArgs != null) {
-                allArgs += defaultArgs.map { it as String }
+        fun executable(executable: Any?) {
+            this.executable.set(executable)
+        }
+
+        @TaskAction
+        fun build() {
+            val allArgs = scriptArgs.toMutableList()
+            if (includeDefaultArgs.getOrElse(true)) {
+                val defaultArgs = project.findProperty("itemis.mps.gradle.ant.defaultScriptArgs") as Collection<*>?
+                if (defaultArgs != null) {
+                    allArgs += defaultArgs.map { it as String }
+                }
             }
-        }
 
-        if(logging.level != null && logging.level != LogLevel.LIFECYCLE && !allArgs.any { it.startsWith("-Dmps.ant.log=") }) {
-            allArgs += "-Dmps.ant.log=${logging.level.toString().toLowerCase(Locale.ENGLISH)}"
-        }
+            if (logging.level != null && logging.level != LogLevel.LIFECYCLE && !allArgs.any { it.startsWith("-Dmps.ant.log=") }) {
+                allArgs += "-Dmps.ant.log=${logging.level.toString().toLowerCase(Locale.ENGLISH)}"
+            }
 
-        if (incremental) {
-            allArgs += "-Dmps.generator.skipUnmodifiedModels=true"
-        }
+            if (incremental) {
+                allArgs += "-Dmps.generator.skipUnmodifiedModels=true"
+            }
 
-        val targets = if (incremental) { targets - "clean" } else { targets }
-
-        project.javaexec {
-            if (this@RunAntScript.executable != null) {
-                executable(this@RunAntScript.executable)
+            val targets = if (incremental) {
+                targets - "clean"
             } else {
-                val defaultJava = project.findProperty("itemis.mps.gradle.ant.defaultJavaExecutable")
-                if (defaultJava != null) {
-                    executable(defaultJava)
+                targets
+            }
+
+            project.javaexec {
+                if (this@RunAntScript.executable.isPresent) {
+                    executable(this@RunAntScript.executable.get())
+                } else {
+                    val defaultJava = project.findProperty("itemis.mps.gradle.ant.defaultJavaExecutable")
+                    if (defaultJava != null) {
+                        executable(defaultJava)
+                    }
                 }
-            }
 
-            mainClass.set("org.apache.tools.ant.launch.Launcher")
-            workingDir = project.rootDir
+                mainClass.set("org.apache.tools.ant.launch.Launcher")
+                workingDir = project.rootDir
 
-            if (includeDefaultClasspath) {
-                val defaultClasspath = project.findProperty(
-                        "itemis.mps.gradle.ant.defaultScriptClasspath") as FileCollection?
-                if (defaultClasspath != null) {
-                    classpath(defaultClasspath)
+                if (includeDefaultClasspath.getOrElse(true)) {
+                    val defaultClasspath = project.findProperty(
+                        "itemis.mps.gradle.ant.defaultScriptClasspath"
+                    ) as FileCollection?
+                    if (defaultClasspath != null) {
+                        classpath(defaultClasspath)
+                    }
                 }
-            }
 
-            if (scriptClasspath != null) {
-                classpath(scriptClasspath)
-            }
+                if (scriptClasspath != null) {
+                    classpath(scriptClasspath)
+                }
 
-            args(allArgs)
-            args("-buildfile", project.file(script))
-            args(targets)
+                args(allArgs)
+                args("-buildfile", project.file(script))
+                args(targets)
+            }
         }
-    }
 }
 
-open class BuildLanguages : RunAntScript() {
+open class BuildLanguages @Inject constructor(of: ObjectFactory) : RunAntScript(of) {
     init {
         targets = listOf("clean", "generate", "assemble")
     }
 }
 
-open class TestLanguages : RunAntScript() {
+open class TestLanguages @Inject constructor(of: ObjectFactory) : RunAntScript(of) {
     init {
         targets = listOf("clean", "generate", "assemble", "check")
     }
